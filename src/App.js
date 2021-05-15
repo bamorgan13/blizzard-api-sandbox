1a996182feb6e5fac851fa2972a960d0eccf8306
Bryce Morgan <bamorgan13@gmail.com>
2021-05-14 19:36:25 -0700

    Add basic react and redux packages. Create redux store and set up standard provider and router structures.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React from 'react';
 
 function App() {
-  return (
-    <h1>Hello world!</h1>
-  );
+	return <h1>Blizz Sandbox</h1>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import configureStore from './store';
+
+const store = configureStore();
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<Provider store={store}>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</Provider>
+	</React.StrictMode>,
+	document.getElementById('root')
 );
