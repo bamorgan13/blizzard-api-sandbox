80f2462df77c056160afef86e438c40c37e88145
Bryce Morgan <bamorgan13@gmail.com>
2021-05-14 23:50:59 -0700

    Refactor CharacterSearchForm into its own component.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { selectAvailableRealms } from './store/selectors';
-import { fetchChar } from './store/characters';
+import { useSelector } from 'react-redux';
 import CharacterHistory from './components/CharacterHistory';
+import CharacterSearchForm from './components/CharacterSearchForm';
 
 function App() {
-	const dispatch = useDispatch();
 	const currentCharKey = useSelector((state) => state.session.currentChar);
 	const currentChar = useSelector((state) => state.characters[currentCharKey]);
 	const [ oAuth, setOAuth ] = useState();
-	const [ region, setRegion ] = useState('us');
-	const [ realm, setRealm ] = useState('');
-	const [ realmOptions, setRealmOptions ] = useState([ { name: '--Select Realm--', slug: '' } ]);
-	const [ name, setName ] = useState('');
 
 	// Retrieve oAuth access token on initial app load
 	useEffect(() => {
@@ -36,58 +30,10 @@ function App() {
 		authorize();
 	}, []);
 
-	// Dynamically fetch the available realms when a new region is selected
-	useEffect(
-		() => {
-			if (oAuth) {
-				async function fetchRealms() {
-					const res = await fetch(
-						`https://${region}.api.blizzard.com/data/wow/realm/index?namespace=dynamic-${region}&locale=en_US&access_token=${oAuth}`
-					);
-					const parsed = await res.json();
-
-					setRealmOptions(selectAvailableRealms(parsed));
-				}
-				fetchRealms();
-			}
-		},
-		[ region, oAuth ]
-	);
-
-	function charSubmit(e) {
-		e.preventDefault();
-		dispatch(fetchChar(region, realm, name.toLowerCase(), oAuth));
-	}
-
 	return (
 		<div>
 			<h1>Blizz Sandbox</h1>
-			<form onSubmit={(e) => charSubmit(e)}>
-				<div className='form-group'>
-					<label htmlFor='region'>Region:</label>
-					<select name='region' value={region} onChange={(e) => setRegion(e.target.value)}>
-						<option value='us'>North America</option>
-						<option value='eu'>Europe</option>
-						<option value='kr'>Korea</option>
-						<option value='tw'>Taiwan</option>
-					</select>
-				</div>
-				<div className='form-group'>
-					<label htmlFor='Realm'>Realm:</label>
-					<select name='realm' value={realm} onChange={(e) => setRealm(e.target.value)}>
-						{realmOptions.map((realmOpt) => (
-							<option value={realmOpt.slug} key={realmOpt.slug}>
-								{realmOpt.name}
-							</option>
-						))}
-					</select>
-				</div>
-				<div className='form-group'>
-					<label htmlFor='name'>Name:</label>
-					<input name='name' type='text' value={name} onChange={(e) => setName(e.target.value)} />
-				</div>
-				<input type='submit' value='Retrieve Character' />
-			</form>
+			<CharacterSearchForm oAuth={oAuth} />
 			{currentChar ? (
 				<div>
 					<p>Name: {currentChar.name}</p>
---
+++ src/components/CharacterSearchForm.js
@@ -0,0 +1,67 @@
+import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { selectAvailableRealms } from '../store/selectors';
+import { fetchChar } from '../store/characters';
+
+function CharacterSearchForm({ oAuth }) {
+	const dispatch = useDispatch();
+
+	const [ region, setRegion ] = useState('us');
+	const [ realm, setRealm ] = useState('');
+	const [ realmOptions, setRealmOptions ] = useState([ { name: '--Select Realm--', slug: '' } ]);
+	const [ name, setName ] = useState('');
+
+	// Dynamically fetch the available realms when a new region is selected
+	useEffect(
+		() => {
+			if (oAuth) {
+				async function fetchRealms() {
+					const res = await fetch(
+						`https://${region}.api.blizzard.com/data/wow/realm/index?namespace=dynamic-${region}&locale=en_US&access_token=${oAuth}`
+					);
+					const parsed = await res.json();
+
+					setRealmOptions(selectAvailableRealms(parsed));
+				}
+				fetchRealms();
+			}
+		},
+		[ region, oAuth ]
+	);
+
+	function charSubmit(e) {
+		e.preventDefault();
+		dispatch(fetchChar(region, realm, name.toLowerCase(), oAuth));
+	}
+
+	return (
+		<form onSubmit={(e) => charSubmit(e)}>
+			<div className='form-group'>
+				<label htmlFor='region'>Region:</label>
+				<select name='region' value={region} onChange={(e) => setRegion(e.target.value)}>
+					<option value='us'>North America</option>
+					<option value='eu'>Europe</option>
+					<option value='kr'>Korea</option>
+					<option value='tw'>Taiwan</option>
+				</select>
+			</div>
+			<div className='form-group'>
+				<label htmlFor='Realm'>Realm:</label>
+				<select name='realm' value={realm} onChange={(e) => setRealm(e.target.value)}>
+					{realmOptions.map((realmOpt) => (
+						<option value={realmOpt.slug} key={realmOpt.slug}>
+							{realmOpt.name}
+						</option>
+					))}
+				</select>
+			</div>
+			<div className='form-group'>
+				<label htmlFor='name'>Name:</label>
+				<input name='name' type='text' value={name} onChange={(e) => setName(e.target.value)} />
+			</div>
+			<input type='submit' value='Retrieve Character' />
+		</form>
+	);
+}
+
+export default CharacterSearchForm;
