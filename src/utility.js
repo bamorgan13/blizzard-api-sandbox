3ed012aef9863e4b60eb8cc70c406e1fd05c5717
Bryce Morgan <bamorgan13@gmail.com>
2021-05-18 02:43:58 -0700

    Update refetch function to only refetch for 429: Too Many Requests

--- src/utility.js
+++ src/utility.js
@@ -2,10 +2,15 @@ export function sleep(ms) {
 	return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-// fetchRetry attempts a refetch after a delay if we do not receive an ok response
+// fetchRetry attempts a refetch after a delay if we receive a 'Too Many Requests'
+// response, otherwise throw the error
 export async function fetchRetry(url, timeout = 500) {
 	const res = await fetch(url);
 	if (res.ok) return res;
-	await sleep(timeout);
-	return fetchRetry(url, timeout);
+	if (res.status === 429) {
+		await sleep(timeout);
+		return fetchRetry(url, timeout);
+	} else {
+		throw new Error(res);
+	}
 }
