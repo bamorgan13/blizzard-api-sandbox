c003b45ea5058b17a074eccad4299b2f01367073
Bryce Morgan <bamorgan13@gmail.com>
2022-09-12 15:27:07 -0700

    Update Header links to open in new tabs

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -16,7 +16,7 @@ function Header() {
 			{
 				authorized ? 
 					<a className='header-auth-link logout' href={`https://battle.net/login/logout?ref=${redirectURI}`}><span className='auth-link logout'>Logout</span> {accountName}</a>
-					: <a className='header-auth-link login' href={`https://oauth.battle.net/authorize?client_id=720e0cff60924f55b9685927ba82d7c8&scope=${encodeURIComponent('openid wow.profile')}&response_type=code&redirect_uri=${redirectURI}`} ><span className='auth-link login'>Login with Battle.net</span></a>
+					: <a className='header-auth-link login' href={`https://oauth.battle.net/authorize?client_id=720e0cff60924f55b9685927ba82d7c8&scope=${encodeURIComponent('openid wow.profile')}&response_type=code&redirect_uri=${redirectURI}`} target='_blank' rel='noreferrer'><span className='auth-link login'>Login with Battle.net</span></a>
 			}
 			<h1
 				data-tip='Return to Welcome Screen'
@@ -34,22 +34,22 @@ function Header() {
 					</div>
 				</li>
 				<li>
-					<a data-tip='Visit my LinkedIn Profile' href='https://www.linkedin.com/in/bryce-morgan-a8792138/'>
+					<a data-tip='Visit my LinkedIn Profile' href='https://www.linkedin.com/in/bryce-morgan-a8792138/' target='_blank' rel='noreferrer'>
 						<div className='logo linkedin' alt='linkedin' />
 					</a>
 				</li>
 				<li>
-					<a data-tip='Visit my personal portfolio site' href='https://www.brycemorgan.dev/'>
+					<a data-tip='Visit my personal portfolio site' href='https://www.brycemorgan.dev/' target='_blank' rel='noreferrer'>
 						<div className='logo laptop' alt='laptop' />
 					</a>
 				</li>
 				<li>
-					<a data-tip='Visit my GitHub Profile' href='https://github.com/bamorgan13'>
+					<a data-tip='Visit my GitHub Profile' href='https://github.com/bamorgan13' target='_blank' rel='noreferrer'>
 						<div className='logo github' alt='github' />
 					</a>
 				</li>
 				<li>
-					<a data-tip='Reach out via email' href='mailto:bamorgan13@gmail.com'>
+					<a data-tip='Reach out via email' href='mailto:bamorgan13@gmail.com' target='_blank' rel='noreferrer'>
 						<div className='logo email' alt='email' />
 					</a>
 				</li>
